2f9f6dd2533a773c44abcf69fc19ffa9d8d22c10
Tomasz Tomczyk <me@tomasztomczyk.com>
2017-08-10 16:27:45 +0100

    First attempt at analog read implementation

--- lib/temperature_sensor.ex
+++ lib/temperature_sensor.ex
@@ -1,3 +1,44 @@
 defmodule TemperatureSensor do
-  
+  alias ElixirALE.GPIO
+
+  @a_pin 18
+  @b_pin 23
+
+  def discharge do
+    {:ok, a_pid} = GPIO.start_link(@a_pin, :input)
+    {:ok, b_pid} = GPIO.start_link(@b_pin, :output)
+
+    GPIO.write(b_pid, 0)
+  end
+
+  def charge_time() do
+    {:ok, a_pid} = GPIO.start_link(@a_pin, :output)
+    {:ok, b_pid} = GPIO.start_link(@b_pin, :input)
+
+    GPIO.write(a_pid, 1)
+
+    current_time = :os.system_time(:millisecond)
+
+    read_gpio(GPIO.read(b_pid), b_pid)
+
+    end_time = :os.system_time(:millisecond)
+
+    end_time - current_time
+  end
+
+  def read_gpio(1,_) do
+    1
+  end
+
+  def read_gpio(_,b_pid) do
+    read_gpio(GPIO.read(b_pid), b_pid)
+  end
+
+  def analog_read do
+    discharge
+    t = charge_time
+    discharge
+
+    t
+  end
 end
